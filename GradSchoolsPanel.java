016a368330465ba04c86580f82e55f32befd73a4
Julia McDonald <juliamcdonald@JuMac.local>
2017-11-19 14:57:38 -0500

    Evaluate

--- GradSchoolsPanel.java
+++ GradSchoolsPanel.java
@@ -5,15 +5,17 @@
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
+import javax.swing.event.*;
 
-public class GradSchoolsPanel extends JPanel {
+public class GradSchoolsPanel extends JPanel implements ChangeListener {
   //instance vars
   private JTabbedPane tabbed_pane;
   private JButton addSchoolutton;
-  private JLabel info, addSchoolInfo, evaluateInfo, schoolLabel, academicsLabel1, researchLabel1, pubsLabel1, 
+  private JLabel info, addSchoolInfo, evaluateInfo, schoolLabel1, academicsLabel1, researchLabel1, pubsLabel1, 
     academicsLabel2, researchLabel2, pubsLabel2;
   private JPanel addSchoolPanel, aboutPanel, evaluatePanel;
   private JTextField schoolNameField;
+  private JSlider acadSlider;
   
   private GradSchools gs;
   
@@ -30,16 +32,18 @@ public class GradSchoolsPanel extends JPanel {
     addSchoolPanel = new JPanel(false);
     tabbed_pane.addTab("Add School", addSchoolPanel);
     
-    evaluatePanel = new JPanel(false);
+    makeEvaluatePanel();
     tabbed_pane.addTab("Evaluate", evaluatePanel);
     
-    schoolNameField = new JTextField(10);
+    
     tabbed_pane.setComponentAt(1, schoolNameField);
     
     add(tabbed_pane);
   }
   
   private void makeAboutPanel() {
+    //we were just testing out GridBagLayout as practice for makeEvaluatePanel() and 
+    //makeAddSchoolPanel(). We realize it is not necessary.
     aboutPanel = new JPanel( new GridBagLayout() );
     
     info = new JLabel( "<html>Choose the Graduate School that fits you best!<br>" +
@@ -55,5 +59,41 @@ public class GradSchoolsPanel extends JPanel {
     aboutPanel.add( info, gbc );
   }
   
+  private void makeEvaluatePanel() {
+    evaluatePanel = new JPanel( new GridBagLayout() );
+    
+    evaluateInfo = new JLabel( "Fill in the information to add a school, then click \"Add School\"" );
+
+    academicsLabel2 = new JLabel( "Academics: " );
+    
+    int min = 0;
+    int max = 5;
+    
+    acadSlider = new JSlider(JSlider.HORIZONTAL, min, max, min);
+    acadSlider.addChangeListener( this );
+    
+    //Turn on labels at major tick marks.
+    acadSlider.setMajorTickSpacing(5);
+    acadSlider.setMinorTickSpacing(1);
+    acadSlider.setPaintTicks(true);
+    acadSlider.setPaintLabels(true);
+    
+    researchLabel2 = new JLabel( "Research: " );
+    
+    pubsLabel2 = new JLabel( "Publications: " );
+    
+    GridBagConstraints gbc = new GridBagConstraints();
+    aboutPanel.add( evaluateInfo, gbc );
+    gbc.gridx++;
+    aboutPanel.add( academicsLabel2, gbc );
+    gbc.gridy++;
+    aboutPanel.add( acadSlider, gbc );
+  }
   
+  public void stateChanged(ChangeEvent e) {
+    JSlider source = (JSlider)e.getSource();
+    if (!source.getValueIsAdjusting()) {
+      int rating = (int)source.getValue();
+    }
+  }
 }
